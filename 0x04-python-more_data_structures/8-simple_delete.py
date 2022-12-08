a7087924e0de25240674b35c4a316c395e243825
Lord <yrnlehjend@gmail.com>
2022-12-08 00:58:32 +0000

    modified edit if statement

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,4 +3,6 @@
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary.keys():
         del a_dictionary[key]
-    return a_dictionary
+        return a_dictionary
+    else:
+        return a_dictionary
