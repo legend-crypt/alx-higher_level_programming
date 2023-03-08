3893fe537658a51e9286c869c601bac4aed67ae0
Lord <yrnlehjend@gmail.com>
2023-03-08 21:37:20 +0000

    fix betty

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdlib.h>
 
 /**
@@ -9,12 +8,11 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
