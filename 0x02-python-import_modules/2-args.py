d9c4c60a4ab8115c4d40ed4dfab3760a986544ce
Lord <yrnlehjend@gmail.com>
2023-03-11 01:19:32 +0000

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,11 +9,11 @@ if argv_len < 2:
     print("{} arguments.".format(argv_len - 1))
 
 elif argv_len < 3:
-    print("{} : argument:".format(argv_len - 1))
+    print("{} argument:".format(argv_len - 1))
     print("{}: {}".format(argv_len - 1, argv[1]))
 
 else:
-    print("{}: arguments:".format(argv_len - 1))
+    print("{} arguments:".format(argv_len - 1))
     for i in range(argv_len):
         if i == 0:
             continue
