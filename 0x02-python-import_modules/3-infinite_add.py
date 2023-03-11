77c54bd8458380906fcf6365b1761fea3d16605b
Lord <yrnlehjend@gmail.com>
2023-03-11 01:48:17 +0000

    restrict import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 import sys
+if __name__ == "__main__":
+    argv = sys.argv
+    argv_len = len(argv)
+    sum = 0
 
-argv = sys.argv
-argv_len = len(argv)
-sum = 0
-
-if argv_len == 1:
-    print(sum)
-else:
-    for i in range(argv_len):
-        if i == 0:
-            continue
-        sum += int(argv[i])
-    print(sum)
+    if argv_len == 1:
+        print(sum)
+    else:
+        for i in range(argv_len):
+            if i == 0:
+                continue
+            sum += int(argv[i])
+        print(sum)
