a9a9837021c3859d19d96de213f92ccaf7e6d23d
Lord <yrnlehjend@gmail.com>
2023-03-11 01:24:44 +0000

    fix import restricion

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
 import sys
+if __name__ == "__main__":
+    argv = sys.argv
+    argv_len = len(argv)
 
-argv = sys.argv
-argv_len = len(argv)
+    if argv_len < 2:
+        print("{} arguments.".format(argv_len - 1))
 
-if argv_len < 2:
-    print("{} arguments.".format(argv_len - 1))
+    elif argv_len < 3:
+        print("{} argument:".format(argv_len - 1))
+        print("{}: {}".format(argv_len - 1, argv[1]))
 
-elif argv_len < 3:
-    print("{} argument:".format(argv_len - 1))
-    print("{}: {}".format(argv_len - 1, argv[1]))
-
-else:
-    print("{} arguments:".format(argv_len - 1))
-    for i in range(argv_len):
-        if i == 0:
-            continue
-        print("{}: {}".format(i, argv[i]))
+    else:
+        print("{} arguments:".format(argv_len - 1))
+        for i in range(argv_len):
+            if i == 0:
+                continue
+            print("{}: {}".format(i, argv[i]))
