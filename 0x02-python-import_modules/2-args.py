5539fac43c3430a207a14e76dbc197d14444ed5c
Lord <yrnlehjend@gmail.com>
2023-03-11 01:14:54 +0000

    command line argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+
+argv = sys.argv
+argv_len = len(argv)
+
+if argv_len < 2:
+    print("{} arguments.".format(argv_len - 1))
+
+elif argv_len < 3:
+    print("{} : argument:".format(argv_len - 1))
+    print("{}: {}".format(argv_len - 1, argv[1]))
+
+else:
+    print("{}: arguments:".format(argv_len - 1))
+    for i in range(argv_len):
+        if i == 0:
+            continue
+        print("{}: {}".format(i, argv[i]))
