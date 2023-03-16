41c80f7514d420bbedeb44edded88f85788c0681
Lord <yrnlehjend@gmail.com>
2023-03-16 18:15:48 +0000

    weight average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    """
+    a function that returns the weighted average of all
+    integers tuple (<score>, <weight>)
+
+    args:
+        my_list: takes in a list
+    Return a the average a weighted list
+
+    """
+    numerator = 1
+    denomerator = 0
+    if len(my_list) == 0:
+        return 0
+    for i in range(len(my_list)):
+        numerator += my_list[i][0] * my_list[i][1]
+    for i in range(len(my_list)):
+        denomerator += my_list[i][1]
+    return numerator / denomerator
