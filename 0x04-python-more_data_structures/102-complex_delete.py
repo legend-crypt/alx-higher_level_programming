8b5d093ed0ea1d33afadb56fe846de793b84e0d2
Lord <yrnlehjend@gmail.com>
2023-03-17 01:18:45 +0000

    delete dictionary field using value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,9 +2,7 @@
 
 def complex_delete(a_dictionary, value):
 
-    for  key, values in a_dictionary.items():
-        if value in values:
-            del a_dictionary[key]
-        else:
-            return a_dictionary
-        return a_dictionary
+    for keys, values in list(a_dictionary.items()):
+        if values == value:
+            del a_dictionary[keys]
+    return a_dictionary
