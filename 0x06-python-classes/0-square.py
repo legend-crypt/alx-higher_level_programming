a297a6808cd1627fdcf64df54594c926ec1cee2c
Lord <yrnlehjend@gmail.com>
2023-03-28 22:57:43 +0000

    fix docs

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+no module imported
+"""
+
 
 class Square:
     """ This an empty class that defines a square """
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+no module imported
+"""
+
 
 class Square:
     """
