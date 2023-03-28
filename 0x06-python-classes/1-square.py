50a5a16c5c0ff8c54f44d06e79aa8cd8027a2b9f
Lord <yrnlehjend@gmail.com>
2023-03-28 20:36:45 +0000

    rename directory

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This class defines a square with
+    """
+    def __init__(self, size):
+        """
+        This intializes the class method
+        Args:
+            size: will intialized as a private attribute
+        """
+        self.__size = size
