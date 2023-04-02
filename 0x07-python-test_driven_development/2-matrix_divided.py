c2bb25433e5e03da80e6c7b5eaa56313a6a80e00
Lord <yrnlehjend@gmail.com>
2023-04-02 00:23:17 +0000

    divide matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+    No module was imported
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+        This function divides each element in a matrix by div
+        and return a new list
+
+        Args:
+            matrix (list): must be a list of lists
+            div (int, float): divides the the matrix
+        Raises:
+            TypeError: if maxtrix is not a list or element are
+                are not int or float
+            ZeroDivisionError: if div is 0
+    """
+
+    new_matrix = matrix.copy()
+
+    try:
+        if not isinstance(matrix, list):
+            raise TypeError("matrix must be a matrix (list of lists) \
+                    of integers/floats")
+        for i in range(len(matrix)):
+            if not isinstance(matrix[i], list):
+                raise TypeError("matrix must be a matrix (list of lists) \
+                        of integers/floats")
+            for a in range(len(matrix[i])):
+                if not isinstance(matrix[i][a], (int, float)):
+                    raise TypeError("matrix must be a matrix (list of lists) \
+                            of integers/floats")
+                elif not isinstance(div, (int, float)):
+                    raise TypeError("div must be a number")
+                else:
+                    new_matrix[i][a] = (round(a / div, 2))
+        return new_matrix
+    except ZeroDivisionError:
+        print("division by 0")
