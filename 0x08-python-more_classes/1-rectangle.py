c9da7f6c3b74bdc5d07959bfbf453967c1a4ad78
Lord <yrnlehjend@gmail.com>
2023-04-03 23:50:46 +0000

    define square with checks

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""
+    No module was imported
+"""
+
+
+class Rectangle:
+
+    """
+        This class define a rectangle with width and height
+    """
+    def __init__(self, width=0, height=0):
+        """
+            Sets up the class
+            with a width and height
+
+            Args:
+                width (int): the widthe of the rectangle
+                height (int): the height of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
