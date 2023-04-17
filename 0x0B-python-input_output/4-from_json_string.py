22b5b53d05f5b864a88e7a22a20c5971f0d85872
Lord <yrnlehjend@gmail.com>
2023-04-17 22:34:08 +0000

    convert from json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+    help convert datatype to json
+"""
+
+import json
+
+
+def from_json_string(my_obj):
+    """
+        converts text to json
+
+        Args:
+            my_obj(json): convert string to json
+    """
+
+    json_repr = json.load(my_obj)
+    return json_repr
