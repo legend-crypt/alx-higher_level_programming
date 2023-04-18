9568c7b330c5fade19661c74a9338bcaa1ce5a29
Lord <yrnlehjend@gmail.com>
2023-04-18 13:45:25 +0000

    create json file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 """
-    help working in json
+    No module was imported
 """
-
-
 def load_from_json_file(filename):
     """
-        Create a new file
+        Create a new file a new json file
+
+        Args:
+            filename: a new file
     """
     with open(filename, mode='w', encoding="utf-8") as file:
-        file.
+        file.close()
