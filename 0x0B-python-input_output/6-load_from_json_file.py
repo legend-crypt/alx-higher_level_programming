d74e2785578448ede58f9592708bd4dbf1dbd162
Lord <yrnlehjend@gmail.com>
2023-04-18 13:43:02 +0000

    create empty file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    help working in json
+"""
+
+
+def load_from_json_file(filename):
+    """
+        Create a new file
+    """
+    with open(filename, mode='w', encoding="utf-8") as file:
+        file.
