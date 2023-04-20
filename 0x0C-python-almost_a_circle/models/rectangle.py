a38303c17e98e653e77018bc941aacb17709879c
Lord <yrnlehjend@gmail.com>
2023-04-20 19:43:19 +0000

    add exception to rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,7 +13,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is not None:
+        if id is not None and id >= 0:
             self.id = id
         else:
             Base.__nb_objects += 1
@@ -39,6 +39,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
@@ -47,6 +51,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
@@ -55,6 +63,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -63,6 +75,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
-
-
