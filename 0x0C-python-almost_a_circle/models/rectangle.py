2643919ad398d4f8260c9112e45333a4661fec03
Lord <yrnlehjend@gmail.com>
2023-04-20 19:23:41 +0000

    fix id instantiation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,7 +27,7 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id=None)
+        super().__init__(id=id)
         self.width = width
         self.height = height
         self.x = x
