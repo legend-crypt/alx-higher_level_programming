6f7cc05b9b64bcdfba3f4e959b89d03875639d61
Lord <yrnlehjend@gmail.com>
2023-04-20 19:15:44 +0000

    create triangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+"""
+    No module was imported
+"""
+
+
+class Base:
+    """
+        This serves as the base class of the
+        other classes
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
+class Rectangle(Base):
+    """
+        This class defines a rectangle and
+        inherit from base class
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id=None)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+
