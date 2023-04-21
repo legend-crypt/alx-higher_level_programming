4bed352f948e48e39054d339f5db28e10f01f029
Lord <yrnlehjend@gmail.com>
2023-04-21 18:35:07 +0000

    add fix import issues

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
 
 """
-    No module was imported
+    imported base class which serves as the
+    base class for rectangle
 """
 
-
-class Base:
-    """
-        This serves as the base class of the
-        other classes
-    """
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        if id is not None and id >= 0:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+from models.base import Base
 
 
 class Rectangle(Base):
