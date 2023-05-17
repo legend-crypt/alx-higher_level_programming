ef79a9aa0d1ef003180eac09a4c3a168a54b0d52
¨lord¨ <yrnlehjend@gmail.com>
2023-05-17 23:14:33 +0000

    add script to find average of scores

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- sql script to remove records <= 5
+-- the name of the the database will be passed as an argument
+DELETE FROM second_table
+WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- a script that calculates the average of the table scores
+-- the name of the database willl be passed as an argument of the mysql command
+SELECT AVG(score)  AS average FROM second_table;
