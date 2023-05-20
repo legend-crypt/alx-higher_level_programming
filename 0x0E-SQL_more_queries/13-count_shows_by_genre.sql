7c4c9ca19396e5d1dce8c22e6c1dfc445ed112be
¨lord¨ <yrnlehjend@gmail.com>
2023-05-20 11:02:39 +0000

    change display name for genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- script to count the number tv show genre
 -- the name of the database will be passed as an argument
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
   FROM tv_genres
  JOIN tv_show_genres
  ON tv_genres.id = tv_show_genres.genre_id
