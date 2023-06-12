e825ccee5223de3430f12b958592468ebc066f65
K Lord <yrnlehjend@gmail.com>
2023-06-12 15:59:58 +0000

    add task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.length === 1) {
+  console.log('No argument');
+} else if (process.argv.length === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
