6b90d03319f59f1f5ac6513a433e589969c8a641
K Lord <yrnlehjend@gmail.com>
2023-06-12 15:33:21 +0000

    add task 2

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const cLang = 'C is fun';
+const pyLang = 'Python is cool';
+const jsLang = 'JavaScript is amazing';
+
+console.log(cLang);
+console.log(pyLang);
+console.log(jsLang);
