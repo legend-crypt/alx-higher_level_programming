07e58fa5a3cad1d78347b6a7cdc4ca3f0aae4834
K Lord <yrnlehjend@gmail.com>
2023-06-18 11:29:53 +0000

    add a script that prevents mysql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    import for working querying data and getting command line args
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
+    except Exception:
+        print("Failed to connect to Databse")
+        exit(0)
+    cur = db.cursor()
+    searched = argv[4]
+    statement = "SELECT * FROM states WHERE name = BINARY %s \
+        ORDER BY states.id ASC"
+    cur.execute(statement, (searched,))
+    row = cur.fetchall()
+    print(row)
+    cur.close()
+    db.close()
