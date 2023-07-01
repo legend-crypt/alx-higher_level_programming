57e730634a56ef476848ff2900d68d90dbae300b
K Lord <yrnlehjend@gmail.com>
2023-07-01 12:01:00 +0000

    add script to issue delete using curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Take a url using command line args and display the size of the body
-
 curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script to request a url using GET
-curl -s "$1"
+curl -sL "$1"
