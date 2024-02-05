994b0891e10ad84d1b3350a4e845aaab49174a1f
lord <konadulordkweku@gmai.com>
2024-02-05 11:34:18 +0000

    fix error

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -16,11 +16,13 @@ def pascal_triangle(n):
             triangle(list): a list of list reprensenting the
                 pascal triangle
     """
+    if n == 0:
+        return []
     triangle = [[1]]
     for i in range(n - 1):
         temp = [0] + triangle[-1] + [0]
         row = []
-        for j in range(len(triangle[-1])+ 1):
+        for j in range(len(triangle[-1]) + 1):
             row.append(temp[j] + temp[j + 1])
         triangle.append(row)
     return triangle
