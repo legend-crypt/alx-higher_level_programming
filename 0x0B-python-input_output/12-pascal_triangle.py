fc6de6961ef1eb12caa9c1693d72c564a3e1bb59
lord <konadulordkweku@gmai.com>
2024-02-05 10:42:22 +0000

    create function to implement pascal triangle given n

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+def pascal_triangle(n):
+    """
+        Returns a list of lists of integers representing
+        the Pascal's triangle of n
+
+        Args:
+            n(int): The pascal coefficient
+        Returns:
+            triangle(list): a list of list reprensenting the
+                pascal triangle
+    """
+    triangle = []
+    for i in range(n):
+        level = 11 ** i
+        level = str(level)
+        values = []
+        for j in level:
+            values.append(j)
+        triangle.append(values)
+    return triangle
