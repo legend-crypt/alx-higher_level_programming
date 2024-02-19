95c8fb87de310be27463085538df95ff06f18d82
lord <konadulordkweku@gmail.com>
2024-02-19 14:33:44 +0000

    fix linting

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,10 +6,8 @@ from sqlalchemy import (
     create_engine, Integer,
     String, Column, ForeignKey
 )
-from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 from sys import argv, exit
-
 
 
 if __name__ == "__main__":
@@ -17,15 +15,18 @@ if __name__ == "__main__":
         print("Input DbUser, Dbpassword and Dbname")
         exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.\
-                           format(argv[1], argv[2],
-                                  argv[3],
-                                  pool_pre_ping=True))
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(argv[1], argv[2],
+               argv[3]),
+        pool_pre_ping=True
+    )
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State.name, City.id, City.name).join(City).\
-            order_by(City.id).all()
+    query = session.query(State.name,
+                          City.id,
+                          City.name).join(City).order_by(City.id).all()
     for state_name, city_id, city_name in query:
         print(f"{state_name}: ({city_id}) {city_name}")
