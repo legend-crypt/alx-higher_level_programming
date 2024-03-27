140747aefc4029f5ad410c3375bb07e28f85da38
lord <konadulordkweku@gmail.com>
2024-03-27 07:07:23 +0000

    fix styling

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@ const request = require('request');
 
 const statusCode = function printStatusCode () {
   const url = process.argv[2];
-  request.get(url, (response, body) => {
-    console.log(`code: ${response && response.statusCode}`);
+  request.get(url, (response) => {
+    console.log('code: ' + response && response.statusCode);
   });
 };
 
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -10,6 +10,6 @@ const streamBody = function streamBody () {
   const url = process.argv[2];
   const filePath = process.argv[3];
   request(url).pipe(fs.createWriteStream(filePath));
-}
+};
 
 streamBody();
