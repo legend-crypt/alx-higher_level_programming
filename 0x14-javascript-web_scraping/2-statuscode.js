06a3985085d065db32851bfbee14eaf27dd8c0ec
lord <konadulordkweku@gmail.com>
2024-03-28 18:05:11 +0000

    add error checking functionalities

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,11 @@ const request = require('request');
 
 const statusCode = function printStatusCode () {
   const url = process.argv[2];
-  request.get(url, (response) => {
-    console.log('code: ' + response && response.statusCode);
+  request.get(url, (err, response) => {
+    if (err) {
+      process.exit(1);
+    }
+    console.log('code: ' + response.statusCode);
   });
 };
 
